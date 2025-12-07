5b31f2a6209157cabf98931891c642831d9d525f
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-12-07 01:04:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function processar(){
     if(veiculo == "c"){
         carro();
     }
-    else if(veiculo == "m") {
+    else if("m") {
         moto();
     }
 }
@@ -28,7 +28,71 @@ function carro(){
             document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${valor*2 - ((valor*2)*10)/100}`;   
         };
     }
-  }
+    else if(hrs === 3){
+        if(mensal === "n"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${(valor*2) + 3}`;   
+        }
+        else if(mensal === "s"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${((valor*2) + 3) - (((valor*2)*10)/100)}`;   
+        };
+    }
+    else if(hrs === 4){
+        if(mensal === "n"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${(valor*2) + 6}`;   
+        }
+        else if(mensal === "s"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${((valor*2) + 6) - (((valor*2)*10)/100)}`;   
+        };
+    }
+    else if(hrs === 5){
+        if(mensal === "n"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${(valor*2) + 9}`;   
+        }
+        else if(mensal === "s"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${((valor*2) + 9) - (((valor*2)*10)/100)}`;   
+        };
+    }
+    else if(hrs === 6){
+        if(mensal === "n"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${(valor*2) + 12}`;   
+        }
+        else if(mensal === "s"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${((valor*2) + 12) - (((valor*2)*10)/100)}`;   
+        };
+    }
+    else if(hrs === 7){
+        if(mensal === "n"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${(valor*2) + 15}`;   
+        }
+        else if(mensal === "s"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${((valor*2) + 15) - (((valor*2)*10)/100)}`;   
+        };
+    }
+    else if(hrs === 8){
+        if(mensal === "n"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${(valor*2) + 18}`;   
+        }
+        else if(mensal === "s"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${((valor*2) + 3) - (((valor*2)*10)/100)}`;   
+        };
+    }
+    else if(hrs === 9){
+        if(mensal === "n"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${(valor*2) + 21}`;   
+        }
+        else if(mensal === "s"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${((valor*2) + 21) - (((valor*2)*10)/100)}`;   
+        };
+    }
+    else if(hrs === 10){
+        if(mensal === "n"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${(valor*2) + 24}`;   
+        }
+        else if(mensal === "s"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${((valor*2) + 24) - (((valor*2)*10)/100)}`;   
+        };
+    }
+}
 
 function moto(){
     let valor = 4;
@@ -48,6 +112,71 @@ function moto(){
         }
         else if(mensal === "s"){
             document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${valor*2 - ((valor*2)*10)/100}`;   
+        };
+    }
+
+    else if(hrs === 3){
+        if(mensal === "n"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${(valor*2) + 2}`;   
+        }
+        else if(mensal === "s"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${((valor*2) + 2) - (((valor*2)*10)/100)}`;   
+        };
+    }
+    else if(hrs === 4){
+        if(mensal === "n"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${(valor*2) + 4}`;   
+        }
+        else if(mensal === "s"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${((valor*2) + 4) - (((valor*2)*10)/100)}`;   
+        };
+    }
+    else if(hrs === 5){
+        if(mensal === "n"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${(valor*2) + 8}`;   
+        }
+        else if(mensal === "s"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${((valor*2) + 8) - (((valor*2)*10)/100)}`;   
+        };
+    }
+    else if(hrs === 6){
+        if(mensal === "n"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${(valor*2) + 10}`;   
+        }
+        else if(mensal === "s"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${((valor*2) + 10) - (((valor*2)*10)/100)}`;   
+        };
+    }
+    else if(hrs === 7){
+        if(mensal === "n"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${(valor*2) + 12}`;   
+        }
+        else if(mensal === "s"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${((valor*2) + 12) - (((valor*2)*10)/100)}`;   
+        };
+    }
+    else if(hrs === 8){
+        if(mensal === "n"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${(valor*2) + 14}`;   
+        }
+        else if(mensal === "s"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${((valor*2) + 14) - (((valor*2)*10)/100)}`;   
+        };
+    }
+    else if(hrs === 9){
+        if(mensal === "n"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${(valor*2) + 16}`;   
+        }
+        else if(mensal === "s"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${((valor*2) + 16) - (((valor*2)*10)/100)}`;   
+        };
+    }
+    else if(hrs === 10){
+        if(mensal === "n"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${(valor*2) + 18}`;   
+        }
+        else if(mensal === "s"){
+            document.getElementById("result").innerText = `Valor a pagar por ${hrs}h = R$ ${((valor*2) + 18) - (((valor*2)*10)/100)}`;   
         };
     }
 }
